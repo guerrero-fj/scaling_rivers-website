72942979df13ced8ffca6832369b3d568ba1f965
Francisco J. Guerrero <97982871+guerrero-fj@users.noreply.github.com>
2022-12-06 19:43:19 -0800

    figures file added and clean up

--- scaling_figures_script.R
+++ scaling_figures_script.R
@@ -15,3 +15,233 @@
 #www.xquartz.org
 
 librarian::shelf(ggplot2,dplyr,plot3D,plot3Drgl,readr,rgl,tidyverse,entropy,GGally)
+
+################################################################################
+# Figures
+################################################################################
+
+# Let's start with a quick pairs plot to have a glimpse of the relationships among
+# variables
+
+ggpairs(bgc_lnd)
+
+
+
+#Land-cover categories (2011)
+thd <- 0.8
+
+
+# Local drainage area
+lnd_m$use_loc <- as.factor(with(lnd_m,ifelse(p_urbn>thd,"Urban",
+                                             ifelse(p_frst>thd,"Forest",
+                                                    ifelse(p_wtnd>thd,"Wetland",
+                                                           ifelse(p_agrc>thd,"Agriculture",
+                                                                  ifelse(p_shrb>thd,"Shrubland",
+                                                                         "Mixed covers")))))))
+
+# Total drainage area
+lnd_m$use_acm <- as.factor(with(lnd_m,ifelse(p_urbn_t>thd,"Urban",
+                                             ifelse(p_frst_t>thd,"Forest",
+                                                    ifelse(p_wtnd_t>thd,"Wetland",
+                                                           ifelse(p_agrc_t>thd,"Agriculture",
+                                                                  ifelse(p_shrb_t>thd,"Shrubland",
+                                                                         "Mixed covers")))))))
+
+
+# Color scale
+
+# Let's define a color scale for our plots using a basic density plot for local 
+# entropies
+
+
+colors <- ggplot_build(lnd_m %>% ggplot(aes(hr_loc, color = use_loc, fill = use_loc))+
+                         geom_density(alpha = 0.5))
+unique(colors$data[[1]]$fill)
+
+my_colors <- c("#F564E3","#00BA38","#F8766D","#B79F00","#619CFF","#00BFC4")
+
+p1 <- ggplot(lnd_m,aes(hr_loc,hr_acm, color = use_acm))+
+  geom_point()+
+  scale_color_manual(values = my_colors)
+p1
+
+p2 <- ggplot(lnd_m, aes(hr_loc, color = use_loc, fill = use_loc))+
+  geom_density(alpha = 0.5)+
+  scale_fill_manual(values = my_colors)+
+  scale_color_manual(values = my_colors)+
+  facet_wrap(~use_loc)
+p2
+
+
+#exploratory plots
+
+# Slope distribution (segments)
+slp.p <- ggplot(dat,aes(TOT_STREAM_SLOPE))+
+  geom_vline(xintercept = 0.01425)+
+  geom_density()+
+  scale_x_log10()
+slp.p
+
+# # Raw slope data
+# dat %>% ggplot(aes(CAT_STREAM_SLOPE, fill = lnd_cat))+
+#   geom_density(alpha = 0.5)+
+#   geom_vline(xintercept = 0.01425, linetype = "dashed")+
+#   scale_x_log10()+
+#   guides(fill = guide_legend(title="Land use type"))
+# 
+# #color scale
+# colors <- ggplot_build(dat %>% ggplot(aes(CAT_STREAM_SLOPE, fill = lnd_cat))+
+#                geom_density(alpha = 0.5)+
+#                geom_vline(xintercept = 0.01425, linetype = "dashed")+
+#                scale_x_log10()+
+#                guides(fill = guide_legend(title="Land use type")))
+# unique(colors$data[[1]]$fill)
+
+# Raw slope data (color scale)
+dat %>% ggplot(aes(TOT_STREAM_SLOPE, fill = lnd_cat))+
+  geom_density(alpha = 0.5)+#scale densities
+  geom_vline(xintercept = 0.01425, linetype = "dashed")+
+  scale_fill_manual(values = c("#F8766D", "#00BF7D","#A3A500", "#00B0F6", "#E76BF3"))+
+  scale_x_log10()+
+  guides(fill = guide_legend(title="Land use type"))
+
+
+# Area distribution (segments)
+are.p <- ggplot(dat,aes(CAT_BASIN_AREA, fill = lnd_cat))+
+  geom_density(alpha = 0.5)+
+  scale_fill_manual(values = c("#F8766D", "#00BF7D","#A3A500", "#00B0F6", "#E76BF3", "gray"))+
+  scale_x_log10()+
+  guides(fill = guide_legend(title="Land use type"))
+are.p
+
+# Area distribution excluding urban and wetlands
+dat %>% 
+  filter(lnd_cat %in% c("Forest", "Agriculture", "Shrubland", "Mixed covers")) %>%
+  ggplot(aes(CAT_BASIN_AREA, fill = lnd_cat))+
+  geom_density(alpha = 0.5)+
+  scale_fill_manual(values = c("#F8766D", "#00BF7D","#A3A500", "#00B0F6", "#E76BF3", "gray"))+
+  scale_x_log10()+
+  guides(fill = guide_legend(title="Land use type"))
+
+
+# Stream length distribution (cumulative at drainage point)
+lgt.p <- ggplot(dat,aes(length_m, fill = lnd_cat))+
+  geom_density(alpha = 0.5)+
+  scale_fill_manual(values = c("#F8766D", "#00BF7D","#A3A500", "#00B0F6", "#E76BF3", "gray"))+
+  scale_x_log10()+
+  guides(fill = guide_legend(title="Land use type"))
+lgt.p
+
+# Stream length distribution excluding urban and wetlands
+dat %>% 
+  filter(lnd_cat %in% c("Forest", "Agriculture", "Shrubland", "Mixed covers")) %>%
+  ggplot(aes(length_m, fill = lnd_cat))+
+  geom_density(alpha = 0.5)+
+  scale_fill_manual(values = c("#F8766D", "#00BF7D","#A3A500", "#00B0F6", "#E76BF3", "gray"))+
+  scale_x_log10()+
+  guides(fill = guide_legend(title="Land use type"))
+
+# Entropy and area
+p2 <- ggplot(dat,aes(AreSqKM,h_rel_3))+
+  geom_point()+
+  scale_x_log10()
+p2
+
+
+
+# Scaling relationships by land cover
+
+## Area and stream length 
+
+dat %>% 
+  filter(lnd_cat %in% c("Forest", "Agriculture", "Shrubland", "Mixed covers")) %>%
+  filter(cum_totco2g_m2_day>0)%>%
+  ggplot(aes(CAT_BASIN_AREA,length_m,color=lnd_cat))+
+  xlab("Watershed Area")+
+  ylab("Stream length")+
+  geom_point(alpha = 0.05)+
+  geom_smooth(method = "lm", se = FALSE)+
+  geom_abline(slope = 1, color = "darkred",linetype = "longdash", size =0.75)+
+  scale_x_log10()+
+  scale_y_log10()+
+  guides(color = guide_legend(title="Land use type"))
+
+
+## Area and Discharge
+
+# Since predicted discharge at a single location is cumulative, it should correlate
+# with the watershed area up to that point
+
+dat %>% 
+  filter(lnd_cat %in% c("Forest", "Agriculture", "Shrubland", "Mixed covers")) %>%
+  filter(cum_totco2g_m2_day>0)%>%
+  ggplot(aes(TOT_BASIN_AREA,logQ_m3_div_s,color=lnd_cat))+
+  geom_abline(slope = 1, color = "darkred",linetype = "longdash", size =0.75)+
+  xlab("Watershed Area")+
+  ylab("Log [Predicted Discharge]")+
+  geom_point(alpha = 0.05)+
+  geom_smooth(method = "lm", se = FALSE)+
+  scale_x_log10()+
+  # scale_y_log10()+
+  guides(color = guide_legend(title="Land use type"))+
+  facet_wrap(~lnd_cat)
+
+## Area and Slope
+
+dat %>% 
+  filter(lnd_cat %in% c("Forest", "Agriculture", "Shrubland", "Mixed covers")) %>%
+  filter(cum_totco2g_m2_day>0)%>%
+  ggplot(aes(TOT_BASIN_AREA,CAT_STREAM_SLOPE,color=lnd_cat))+
+  xlab("Watershed Area")+
+  ylab("Local Stream Slope")+
+  geom_point(alpha = 0.05)+
+  geom_smooth(method = "lm",se = FALSE, span = 0.5)+
+  scale_x_log10()+
+  scale_y_log10()+
+  guides(color = guide_legend(title="Land use type"))+
+  facet_wrap(~lnd_cat)
+
+# Watershed area and cummulative metabolic rate
+
+dat %>% 
+  filter(lnd_cat %in% c("Forest", "Agriculture", "Shrubland", "Mixed covers")) %>%
+  filter(cum_totco2g_m2_day>0)%>%
+  ggplot(aes(TOT_BASIN_AREA,cum_totco2g_m2_day,color=lnd_cat))+
+  xlab("Watershed Area")+
+  ylab("Cummulative respiration rates")+
+  geom_point(alpha = 0.05)+
+  geom_smooth(method = "lm")+
+  geom_abline(slope = 1, color = "darkred",linetype = "longdash", size =0.75)+
+  scale_x_log10()+
+  scale_y_log10()+
+  facet_wrap(~lnd_cat)+
+  theme(legend.position = "none",
+        strip.background = element_blank(),
+        strip.text.x = element_text(size = 12, hjust = 0),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.title = element_text(size=14))
+
+# Role of landscape heterogeneity
+
+dat %>% 
+  filter(lnd_cat %in% c("Mixed covers")) %>%
+  filter(cum_totco2g_m2_day>0)%>%
+  ggplot(aes(TOT_BASIN_AREA,cum_totco2g_m2_day, color = h_rel_3))+
+  xlab("Watershed Area")+
+  ylab("Cummulative respiration rates")+
+  # geom_point(alpha = 0.05)+
+  geom_point()+
+  geom_abline(slope = 1, color = "darkred",linetype = "longdash", size =0.75)+
+  scale_x_log10()+
+  scale_y_log10()
+
+# Entropy plot
+
+with(dat,scatter3D(tot_other_3,tot_ntrl_3,h_rel_3,bty ='b2',
+                   colvar = as.integer(lnd_cat),
+                   clab=c("Relative","Entropy"), 
+                   theta = 15, phi =20, main = "Landscape Heterogeneity", 
+                   xlab = "Mixed cover", ylab = "Natural cover",
+                   zlab = "Relative entropy"))
+plotrgl()
